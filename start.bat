6eee1a7c1641e05068ff6fc481e45c8c5adc187f
Andrew Stanton <AndrewStanton94@users.noreply.github.com>
2017-11-30 16:41:31 +0000

    Pauses to log in before opening all pages
    There is a 60 second timeout after the ITH & printer pages are opened.
    This allows the user to log in, then the required pages can be opened.

--- start.bat
+++ start.bat
@@ -1,3 +1,11 @@
 @echo off
-start chrome "http://ith.port.ac.uk/" "http://printers.port.ac.uk:9191/app" "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://is-ssddg-app-01.uni.ds.port.ac.uk/traka/v2/web/" "https://mail.google.com/mail/u/0/#inbox" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/" "https://www.messenger.com/t/1097815366974128"
+:: Log on for rota (both sites) & printer
+start chrome "http://ith.port.ac.uk/" "http://is-web-02.uni.ds.port.ac.uk" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease"
+
 start notepad
+
+:: Delay in seconds
+timeout 60
+
+:: Opens all web pages
+start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://is-web-02.uni.ds.port.ac.uk/dashboardnew" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease" "http://is-ssddg-app-01.uni.ds.port.ac.uk/traka/v2/web/" "https://mail.google.com/mail/u/0/#inbox" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/" "https://www.messenger.com/t/1097815366974128"
