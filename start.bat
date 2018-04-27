a027f9b9a06250f674a5d1b224246cd46c5bdb5a
Andrew Stanton <AndrewStanton94@users.noreply.github.com>
2018-04-27 05:55:29 +0100

    Added mymaps

--- start.bat
+++ start.bat
@@ -6,7 +6,7 @@ start chrome "http://is-web-02.uni.ds.port.ac.uk" "http://printers.port.ac.uk:91
 timeout 75
 
 :: Opens all web pages
-start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease" "https://mail.google.com/mail/u/0/#inbox" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/sso" "https://www.messenger.com/t/1097815366974128"
+start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease" "https://mail.google.com/mail/u/0/#inbox" "https://ssd.port.ac.uk/mymaps/" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/sso" "https://www.messenger.com/t/1097815366974128"
 
 start notepad
 
