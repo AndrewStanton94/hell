4e60fb867888739a75ee1963423c68da61e9ad27
Andrew Stanton <AndrewStanton94@users.noreply.github.com>
2018-04-27 05:51:19 +0100

    Transition insurance
    Links to lost property and editable rota

--- start.bat
+++ start.bat
@@ -1,11 +1,14 @@
 @echo off
 :: Log on for rota (both sites) & printer
-start chrome "http://ith.port.ac.uk/" "http://is-web-02.uni.ds.port.ac.uk" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease"
-
-start notepad
+start chrome "http://is-web-02.uni.ds.port.ac.uk" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease"
 
 :: Delay in seconds
 timeout 75
 
 :: Opens all web pages
 start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease" "https://mail.google.com/mail/u/0/#inbox" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/sso" "https://www.messenger.com/t/1097815366974128"
+
+start notepad
+
+:: Transition insurance
+start chrome "http://is-web-01.uni.ds.port.ac.uk/" "http://is-web-02.uni.ds.port.ac.uk/app/rotaold"
