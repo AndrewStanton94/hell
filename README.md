430bf3236d93828a3a671884798ee81e83964d6b
Andrew Stanton <AndrewStanton94@users.noreply.github.com>
2018-05-11 07:01:28 +0100

    Added windows 10 issue logger page (#3)
    * Added windows 10 issue logger page
    * Updated docs and made all items into links

--- README.md
+++ README.md
@@ -5,17 +5,18 @@ A collection of .bat files (name is a song reference)
 *Links to the [editable rota](http://is-web-02.uni.ds.port.ac.uk/app/rotaold) available while required*
 
 This opens the login pages for
-+ ITH website
++ [ITH website](http://is-web-02.uni.ds.port.ac.uk/)
 + Papercut
 
 After a 75 second delay it will open
 + The rota
-+ Papercut pending release page
-+ appsanywhere
-+ gmail
-+ mymaps
-+ the library classmark tool
-+ The ITH group chat in messenger
++ [Papercut *pending release* page](http://printers.port.ac.uk:9191/app?service=page/PrinterRelease)
++ [appsanywhere](https://appsanywhere.port.ac.uk/sso)
++ [gmail](https://mail.google.com/mail/u/0)
++ [mymaps](https://ssd.port.ac.uk/mymaps/)
++ [Library classmark tool](https://library.port.ac.uk/classmark/)
++ [The ITH group chat in messenger](https://www.messenger.com/t/1097815366974128)
++ [Windows 10 issue logger](https://servicedesk.port.ac.uk/sw/selfservice/portal.php#wiz=Windows+10+Feedback)
 
 It also opens notepad
 
--- start.bat
+++ start.bat
@@ -1,12 +1,12 @@
 @echo off
-:: Log on for rota (both sites) & printer
+:: Log on for rota & printer
 start chrome "http://is-web-02.uni.ds.port.ac.uk" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease"
 
 :: Delay in seconds
 timeout 75
 
 :: Opens all web pages
-start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease" "https://mail.google.com/mail/u/0/#inbox" "https://ssd.port.ac.uk/mymaps/" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/sso" "https://www.messenger.com/t/1097815366974128"
+start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease" "https://mail.google.com/mail/u/0/#inbox" "https://ssd.port.ac.uk/mymaps/" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/sso" "https://www.messenger.com/t/1097815366974128" "https://servicedesk.port.ac.uk/sw/selfservice/portal.php#wiz=Windows+10+Feedback"
 
 start notepad
 
