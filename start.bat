87521b246b32b6871defd1e8b62582c6a3d9721e
Andrew Stanton <AndrewStanton94@users.noreply.github.com>
2018-08-06 18:45:27 +0100

    Opens ALT, removes rotaold, second papercut and win10 feedback (#4)
    * Opens ALT, removes rotaold & second papercut
    
    The link to ALT on the ITH website no longer works.
    The conversion to the current rota seems fine so [rotaold](http://is-web-02.uni.ds.port.ac.uk/app/rotaold) is no longer needed on start up
    The latest version of PaperCut is able to redirect to the intended page on login. So the link once to login and again to get the required page is no longer needed.
    
    * Update README.md

--- start.bat
+++ start.bat
@@ -6,9 +6,6 @@ start chrome "http://is-web-02.uni.ds.port.ac.uk" "http://printers.port.ac.uk:91
 timeout 75
 
 :: Opens all web pages
-start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota" "http://printers.port.ac.uk:9191/app?service=page/PrinterRelease" "https://mail.google.com/mail/u/0/#inbox" "https://ssd.port.ac.uk/mymaps/" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/sso" "https://www.messenger.com/t/1097815366974128" "https://servicedesk.port.ac.uk/sw/selfservice/portal.php#wiz=Windows+10+Feedback"
+start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rota"  "https://sdtools.port.ac.uk/alt-v4" "https://mail.google.com/mail/u/0/#inbox" "https://ssd.port.ac.uk/mymaps/" "https://library.port.ac.uk/classmark/" "https://appsanywhere.port.ac.uk/sso" "https://www.messenger.com/t/1097815366974128"
 
 start notepad
-
-:: Transition insurance
-start chrome "http://is-web-02.uni.ds.port.ac.uk/app/rotaold"
